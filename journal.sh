d62d30ab8d7c88e10d51a6cf60855bb6d41cbd05
Marek Pazik <poczta@marekpazik.pl>
2016-08-07 21:24:29 +0200

    Changing indentation size to 4 spaces.

--- journal.sh
+++ journal.sh
@@ -2,34 +2,34 @@
 # A script that manages daily journal
 
 die() {
-  echo "$*"
-  exit 1
+    echo "$*"
+    exit 1
 }
 
 import_config() {
-  source ./journal.cfg
-  [ -z "$JOURNAL_PATH" ] && die "JOURNAL_PATH has to be defined"
-  [ -z "$JOURNAL_EDITOR" ] && die "JOURNAL_EDITOR has to be defined"
+    source ./journal.cfg
+    [ -z "$JOURNAL_PATH" ] && die "JOURNAL_PATH has to be defined"
+    [ -z "$JOURNAL_EDITOR" ] && die "JOURNAL_EDITOR has to be defined"
 }
 
 today_date() {
-  echo $(date +%Y-%m-%d)
+    echo $(date +%Y-%m-%d)
 }
 
 file_name() {
-  local date=$1
-  local extension=$([ -z "$JOURNAL_FILE_EXTENSION" ] && echo "" || echo ".${JOURNAL_FILE_EXTENSION}")
+    local date=$1
+    local extension=$([ -z "$JOURNAL_FILE_EXTENSION" ] && echo "" || echo ".${JOURNAL_FILE_EXTENSION}")
 
-  echo ${date}${extension}
+    echo ${date}${extension}
 }
 
 file_path() {
-  local date=$1
-  echo "${JOURNAL_PATH}/$(file_name ${date})"
+    local date=$1
+    echo "${JOURNAL_PATH}/$(file_name ${date})"
 }
 
 main() {
-  import_config
-  file_path $(today_date)
+    import_config
+    file_path $(today_date)
 }
 main
