3ad220795fb57d8f9a7febd847f2372def8ba635
Marek Pazik <poczta@marekpazik.pl>
2016-08-07 20:59:04 +0200

    Config file import.

--- journal.sh
+++ journal.sh
@@ -1,1 +1,19 @@
+#!/usr/bin/env bash
+# A script that manages daily journal
 
+die() {
+  echo "$*"
+  exit 1
+}
+
+import_config() {
+  source ./journal.cfg
+  [ -z "$JOURNAL_PATH" ] && die "JOURNAL_PATH has to be defined"
+  [ -z "$JOURNAL_EDITOR" ] && die "JOURNAL_EDITOR has to be defined"
+}
+
+main() {
+  import_config
+  echo ${JOURNAL_EDITOR}
+}
+main
